5d5254e05a2459eb78d5ef1ea2e97318710d8faf
shrimp78 <y.kitamura78@gmail.com>
2016-02-26 11:47:34 +0900

    割引率のハッシュを追加

--- cal.rb
+++ cal.rb
@@ -1,4 +1,6 @@
 TAX = {"北海道" => 0.0685, "東日本" => 0.08, "西日本" => 0.0625, "四国" => 0.04, "九州" => 0.0825}
+DISCOUNT = {1000 => 0.03, 5000 => 0.05, 7000 => 0.07, 10000 => 0.10, 50000 => 0.15}
+
 price_str = ARGV[0]
 
 num_str = ARGV[1]
@@ -8,9 +10,11 @@ price = price_str.to_i
 num = num_str.to_i
 sum = price * num
 tax = TAX[area]
+amount = sum * ( 1 + tax)
 
 puts "price:#{price}" 
 puts "num:#{num}" 
 puts "area:#{area}"
 puts "sum:#{sum}"
 puts "tax:#{tax}"
+puts "amount:#{amount}"
