219f3e41d8bbfec5aa6d7a1922a42a577efcf974
tomita-j@nttr.co.jp <tomita-j@nttlab-10-pro-muradev-001.ms.internal>
2016-02-26 12:00:09 +0900

    割引率修正

--- cal.rb
+++ cal.rb
@@ -1,8 +1,23 @@
 TAX = {"北海道" => 0.0685, "東日本" => 0.08, "西日本" => 0.0625, "四国" => 0.04, "九州" => 0.0825}
 DISCOUNT = {1000 => 0.03, 5000 => 0.05, 7000 => 0.07, 10000 => 0.10, 50000 => 0.15}
 
-price_str = ARGV[0]
+def discount_rate sum
+  if (sum < 1000)
+    return 0
+  elsif ( sum < 5000 )
+    return 0.03
+  elsif ( sum < 7000 )
+    return 0.05
+  elsif ( sum < 10000 )
+    return 0.07
+  elsif ( sum < 50000 )
+    return 0.1
+  else
+    return 0.15
+  end
+end
 
+price_str = ARGV[0]
 num_str = ARGV[1]
 area = ARGV[2]
 
@@ -10,8 +25,9 @@ price = price_str.to_i
 num = num_str.to_i
 sum = price * num
 tax = TAX[area]
-discount = DISCOUNT[price]
+discount = discount_rate(sum)
 amount = sum * ( 1 + tax)
+
 
 puts "price:#{price}" 
 puts "num:#{num}" 
@@ -20,3 +36,19 @@ puts "sum:#{sum}"
 puts "tax:#{tax}"
 puts "amount:#{amount}"
 puts "discount#{discount}"
+
+def discount_rate sum 
+  if (sum < 1000)
+    return 0
+  elsif ( sum < 5000 )
+    return 0.03
+  elsif ( sum < 7000 )
+    return 0.05
+  elsif ( sum < 10000 )
+    return 0.07
+  elsif ( sum < 50000 ) 
+    return 0.1
+  else 
+    return 0.15
+  end
+end
